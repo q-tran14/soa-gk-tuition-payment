6dd75bc9683f99d0140fb68b69665163ad323ffa
q-tran14 <tranquangquan.it@gmail.com>
2025-09-24 10:14:36 +0700

    Update database connect file

--- service-js/configs/database.js
+++ service-js/configs/database.js
@@ -1,16 +1,31 @@
-const sql = require("mssql");
+const sql = require("mssql/msnodesqlv8");
 
-const config = {
-  server: process.env.DB_SERVER,   // 👈 bắt buộc có
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT, 10),
-  options: {
-    encrypt: false,
-    trustServerCertificate: true,
-  },
-};
+let config;
+
+if (process.env.DB_USER && process.env.DB_PASSWORD) {
+  // SQL Authentication
+  config = {
+    server: process.env.DB_SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    options: {
+      encrypt: false,
+      trustServerCertificate: true,
+    },
+  };
+} else {
+  // Windows Authentication
+  config = {
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    driver: "msnodesqlv8",
+    options: {
+      trustedConnection: true,
+      trustServerCertificate: true,
+    },
+  };
+}
 
 const pool = new sql.ConnectionPool(config);
 
